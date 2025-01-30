ea31abc3848c2640a1ebb5447fbb50ac30fc7aa9
Denis <fpv.airplane.barnaul@gmail.com>
2025-01-30 20:13:10 +0300

    Initial Commit

---
+++ SUAIHelper/SUAIHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUAIHelperApp.swift
+//  SUAIHelper
+//
+//  Created by Denis on 30.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct SUAIHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
